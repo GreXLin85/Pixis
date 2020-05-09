1fa88e1def8a4c02de21883355c96494825baa52
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2020-05-09 16:24:23 +0300

    Version : Pixel Destroyer
    [*]Fixed Bugs

--- Core/Downloader.js
+++ Core/Downloader.js
@@ -1,42 +1,27 @@
-var request = require('request');
-var progress = require('request-progress');
+const request = require('request');
+const progress = require('request-progress');
 const ProgressBar = require('progress');
 
-var bar = new ProgressBar('[:bar] :percent', {
-    complete: '=',
-    incomplete: ' ',
-    width:
-        100
-    ,
-    total: 1
-});
-
 module.exports = (link, filename, location) => {
+    var bar = new ProgressBar(filename + ' [:bar] :percent :seconds sec ', {
+        complete: '=',
+        incomplete: ' ',
+        width: 100,
+        total: 50
+    });
 
-    progress(request(link), {
-        // throttle: 2000,                    // Throttle the progress event to 2000ms, defaults to 1000ms
-        // delay: 1000,                       // Only start to emit after 1000ms delay, defaults to 0ms
-        // lengthHeader: 'x-transfer-length'  // Length header to use, defaults to content-length
-    })
+    progress(request(link))
         .on('progress', function (state) {
-            // The state is an object that looks like this:
-            // {
-            //     percent: 0.5,               // Overall percent (between 0 to 1)
-            //     speed: 554732,              // The download speed in bytes/sec
-            //     size: {
-            //         total: 90044871,        // The total payload size in bytes
-            //         transferred: 27610959   // The transferred payload size in bytes
-            //     },
-            //     time: {
-            //         elapsed: 36.235,        // The total elapsed seconds since the start (3 decimals)
-            //         remaining: 81.403       // The remaining seconds to finish (3 decimals)
-            //     }
-            // }
-            bar = new ProgressBar(filename+' [:bar] :percent ', {
+            bar = new ProgressBar(filename + ' [:bar] :percent :seconds sec ', {
+                complete: '=',
+                incomplete: ' ',
+                width: 100,
                 total: state.size.total
             });
 
-            bar.tick(state.size.transferred)
+            bar.tick(state.size.transferred, {
+                'seconds': state.time.remaining
+            })
         })
         .on('error', function (err) {
             console.log("[!]There is a error\n" + err)
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const argv = require("yargs").argv
-var readlineSync = require('readline-sync');
-
+const readlineSync = require('readline-sync');
+const {existsSync,mkdirSync} = require("fs")
 const { argvCheckButRequire, HelpText, Downloader, getFileNameFromLink } = require("./Core/Core")
 
 /**
@@ -9,21 +9,48 @@ const { argvCheckButRequire, HelpText, Downloader, getFileNameFromLink } = requi
  * --link (require)
  */
 
-var link = argv.link
+const link = argv.link
 
 if (argv.help == true) {
     console.log(HelpText)
 }
 
 if (argvCheckButRequire(argv.link)) {
+    if (!existsSync("./downloads")) {
+        mkdirSync("./downloads")
+    }
+
     try {
         var filename = getFileNameFromLink(argv.link)
-        console.log("[*]Note for file name : If you are want to select default file name press to enter.")
-        var FileNameq = readlineSync.question('[?]File Name (' + filename + ') : ')
-        if (FileNameq != "") {
-            filename = FileNameq
+
+        if (filename == "") {
+            while (true) {
+                var FileNameq = readlineSync.question('[?]File Name : ')
+                if (FileNameq != "") {
+                    if (/([^\\/:*?<>|])$/.test(FileNameq)) {
+                        filename = FileNameq
+                        break
+                    } else {
+                        console.log("You can't use this characters \\/:*?<>|")
+                    }
+                } else {
+                    console.log("You must define file name ([Example] : example.txt)")
+                }
+            }
+
+            console.log(filename + "\n" + FileNameq)
+        } else {
+            console.log("[*]Note for file name : If you are want to select default file name press to enter.")
+            var FileNameq = readlineSync.question('[?]File Name (' + filename + ') : ')
+            if (FileNameq != "") {
+                if (/([^\\/:*?<>|])$/.test(FileNameq)) {
+                    filename = FileNameq
+                } else {
+                    console.log("You can't use this characters \\/:*?<>|")
+                }
+            }
+            console.log(filename + "\n" + FileNameq)
         }
-        console.log(filename + "\n" + FileNameq)
 
         var downloadlocation = __dirname + '/downloads/'
         console.log("\n[*]Note for download location : If you are want to select default file name press to enter.")
